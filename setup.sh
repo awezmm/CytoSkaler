24c499e0101f07868523a5daf0df98509817eb54
awezmm <42984176+awezmm@users.noreply.github.com>
2021-08-14 23:07:24 -0500

    multiple distribution folder check
     zshell path append

--- setup.sh
+++ setup.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-curl https://people.cs.uchicago.edu/~aamohsin/distribution.tar.gz > distribution.tar.gz
+DIR="distribution/"
+if [ -d "$DIR" ]; then
+   echo "Error! A 'distribution' folder already exists in this directory. Either delete the 'distribution' folder or setup this program in a different directory."
+   exit 1
+fi
 
-tar xvzf distribution.tar.gz
 
-rm distribution.tar.gz
-
-cd distribution
-
-mv * ../
-
-cd ..
-
-rm -rf distribution
+curl https://people.cs.uchicago.edu/~aamohsin/distribution.tar.gz > distribution.tar.gz && tar xvzf distribution.tar.gz && rm distribution.tar.gz && cd distribution && mv * ../ && cd .. && rm -rf distribution
 
 echo " " >> ~/.bashrc
 echo "# MATLAB 2019A Runtime Path Append" >> ~/.bashrc
@@ -31,6 +26,11 @@ echo " " >> ~/.bash_profile
 echo "# MATLAB 2019A Runtime Path Append" >> ~/.bash_profile
 echo "export DYLD_LIBRARY_PATH=/Applications/MATLAB/MATLAB_Runtime/v96/runtime/maci64:/Applications/MATLAB/MATLAB_Runtime/v96/sys/os/maci64:/Applications/MATLAB/MATLAB_Runtime/v96/bin/maci64:/Applications/MATLAB/MATLAB_Runtime/v96/extern/bin/maci64" >> ~/.bash_profile
 echo " " >> ~/.bash_profile
+
+echo " " >> ~/.zshenv
+echo "# MATLAB 2019A Runtime Path Append" >> ~/.zshenv
+echo "export DYLD_LIBRARY_PATH=/Applications/MATLAB/MATLAB_Runtime/v96/runtime/maci64:/Applications/MATLAB/MATLAB_Runtime/v96/sys/os/maci64:/Applications/MATLAB/MATLAB_Runtime/v96/bin/maci64:/Applications/MATLAB/MATLAB_Runtime/v96/extern/bin/maci64" >> ~/.zshenv
+echo " " >> ~/.zshenv
 
 
 pwd > ~/.CytoSkalerInfo
